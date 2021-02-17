23b63dd21161ea1d1e605afeb378d437f1197c27
unknown <lenguyenngocluong.com>
2021-02-17 20:40:14 +0700

    demo

---
+++ myjs.js
@@ -0,0 +1,33 @@
+/*let sum=0;
+for(let i=1;i<=100;i++){
+    sum = sum + i;
+}
+document.write(sum);*/
+
+/*
+//Tính tổng các số chia hết cho 3 từ 1-100
+
+let sum=0;
+for(let i=1;i<=100;i++){
+    if(i%3==0){
+        sum=sum+i;
+    }
+}
+document.write(sum);*/
+
+/*
+//Tính tổng các số chia hết cho 3 nhưng không chia hết cho 9 từ 1-100
+let sum=0;
+for(i=1;i<=100;i++){
+    if(i%3==0 && i%9!==0){
+        sum= sum+i;
+    }
+}
+document.write(i);*/
+
+//Hãy viết một chương trình in ra các số từ 1 đến 100. Nhưng nếu số chia hết cho 3 thì in ra “Fizz”,5 thì in ra “Buzz” thay vì in ra số đó. Và nếu số đó chia hết cho cả 3 và 5 thì in ra chữ “FizzBuzz”.
+for(let i=1,i<=100,i++){
+    
+}
+
+
